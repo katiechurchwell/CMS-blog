b3b8aca73169aa37c953960aacdde633d00f37d8
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-15 16:55:51 -0800

    secret

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'superdupersecret',
+  secret: "superdupersecret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
